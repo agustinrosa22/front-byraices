2f834cff6d420c0d720967aa3ea7fb559158a577
MatheoAlbarracin <matheoalbarra@gmail.com>
2024-05-06 12:23:48 -0300

    subida a repo

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,44 +1,42 @@
 import React from "react";
-import style from './Home.module.css'
-import Lupa from '../../Assets/lupa.png'
+import style from './Home.module.css';
+import Lupa from '../../Assets/lupa.png';
 
-const Home = ()=>{
-    return(
+const Home = () => {
+    return (
         <div className={style.container}>
-        <div className={style.intro}>
-
-        <h1 className={style.title}>Mucho más que mudarte</h1>
-        <div className={style.buttonsContainer}>
-            <button className={style.button} onClick={() => window.location.href='/buy'}>Quiero comprar</button>
-            <button className={style.button} onClick={() => window.location.href='/alquilar'}>Quiero alquilar</button>
-            <button className={style.button} onClick={() => window.location.href='/sell'}>Quiero vender</button>
-            <button className={style.button} onClick={() => window.location.href='/emprendimientos'}>Desarrollo</button>
-        </div>
-        <div className={style.searchContainer}>
-            <input type="text" className={style.searchInput} placeholder="¿Dónde quieres mudarte?" />
-            <select className={style.propertyTypeDropdown}>
-                <option value="departamento">Departamento</option>
-                <option value="casa">Casa</option>
-                <option value="ph">PH</option>
-                <option value="local">Local</option>
-                <option value="terrenos y lotes">Terrenos y lotes</option>
-                <option value="campos y chacras">Campos y chacras</option>
-                <option value="fondo de comercio">Fondo de comercio</option>
-                <option value="cochera">Cochera</option>
-                <option value="oficina">Oficina</option>
-                <option value="galpon">Galpón</option>
-                <option value="quinta">Quinta</option>
-                <option value="otros">Otros</option>
-            </select>
-            <button className={style.searchButton}>
+            <div className={style.intro}>
+                <h1 className={style.title}>Mucho más que mudarte</h1>
+                <div className={style.buttonsContainer}>
+                    <button className={style.button} onClick={() => window.location.href = '/buy'}>Comprar</button>
+                    <button className={style.button} onClick={() => window.location.href = '/alquilar'}>Alquilar</button>
+                    <button className={style.button} onClick={() => window.location.href = '/sell'}>Vender</button>
+                    <button className={style.button} onClick={() => window.location.href = '/emprendimientos'}>Desarrollos</button>
+                </div>
+                <div className={style.searchContainer}>
+                    <input type="text" className={style.searchInput} placeholder="¿Dónde quieres mudarte?" />
+                    <select className={style.propertyTypeDropdown}>
+                        <option value="departamento">Departamento</option>
+                        <option value="casa">Casa</option>
+                        <option value="ph">PH</option>
+                        <option value="local">Local</option>
+                        <option value="terrenos y lotes">Terrenos y lotes</option>
+                        <option value="campos y chacras">Campos y chacras</option>
+                        <option value="fondo de comercio">Fondo de comercio</option>
+                        <option value="cochera">Cochera</option>
+                        <option value="oficina">Oficina</option>
+                        <option value="galpon">Galpón</option>
+                        <option value="quinta">Quinta</option>
+                        <option value="otros">Otros</option>
+                    </select>
+                    <button className={style.searchButton}>
                         <img src={Lupa} alt="Buscar" />
                     </button>
+                </div>
+            </div>
+            <div>
+            </div>
         </div>
-        </div>
-        <div>
-            
-        </div>
-    </div>
     );
 };
 
