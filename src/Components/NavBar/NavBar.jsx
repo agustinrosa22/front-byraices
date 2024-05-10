db3341af444fa8ee005ef310f14d9b788a0367c9
agustinrosa22 <agustinrosa1234@gmail.com>
2024-05-10 14:50:21 -0300

    navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,53 +1,70 @@
-import React, { useState }  from "react";
-import styles from './NavBar.module.css'
-import { NavLink } from "react-router-dom";
-import logo from '../../Assets/titulo.png'
+import React, { useState } from "react";
+import { Navbar, Nav } from 'react-bootstrap';
+import logo from '../../Assets/titulo.png';
+import styles from './NavBar.module.css';
 
-const NavBar = ()=> {
+const NavBar = () => {
     const [activeLink, setActiveLink] = useState(null);
 
     const handleNavLinkClick = (link) => {
         setActiveLink(link);
+        console.log("activeLink actualizado:", link);
     };
-    return(
-        <div className={styles.navContainer}>
-        <nav className={styles.navbar}>
-            <div className={styles.leftSection}>
-                <img src={logo} alt="" className={styles.img}/>
-            </div>
-            <div className={styles.navLinks}>
-                <NavLink to='/' activeClassName={styles.activeLink} exact onClick={() => handleNavLinkClick('home')} className={activeLink === 'home' ? styles.active : ''}>
-                    Home
-                </NavLink>
-                <NavLink to='/sale' activeClassName={styles.activeLink} onClick={() => handleNavLinkClick('comprar')} className={activeLink === 'comprar' ? styles.active : ''}>
-                    Comprar
-                </NavLink>
-                <NavLink to='/sell' activeClassName={styles.activeLink} onClick={() => handleNavLinkClick('vender')} className={activeLink === 'vender' ? styles.active : ''}>
-                    Vender
-                </NavLink>
-                <NavLink to='/rent' activeClassName={styles.activeLink} onClick={() => handleNavLinkClick('alquilar')} className={activeLink === 'alquilar' ? styles.active : ''}>
-                    Alquilar
-                </NavLink>
-                <NavLink to='/desarrollo' activeClassName={styles.activeLink} onClick={() => handleNavLinkClick('desarrollo')} className={activeLink === 'desarrollo' ? styles.active : ''}>
-                    Desarrollos
-                </NavLink>
 
-                <NavLink to='/franquicias' activeClassName={styles.activeLink} onClick={() => handleNavLinkClick('franquicias')} className={activeLink === 'franquicias' ? styles.active : ''}>
-                    Franquicias
-                </NavLink>
-
-                <NavLink to='/contact' activeClassName={styles.activeLink} onClick={() => handleNavLinkClick('contacto')} className={activeLink === 'contacto' ? styles.active : ''}>
-                    Contacto
-                </NavLink>
-            </div>
-            <div className={styles.rightSection}>
-                <NavLink to='/login' activeClassName={styles.activeLink} className={styles.butonexit}>
-                    Iniciar Sesión
-                </NavLink>
-            </div>
-        </nav>
-    </div>
-    )
+    return (
+        <Navbar className={styles.navbar} collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar.Brand href="/">
+                <img src={logo} alt="" className={styles.img} />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse className={styles.navCenter} style={{ justifyContent: 'center' }} id="responsive-navbar-nav">
+                <Nav className={`mx-auto ${styles.navCenter}`}> {/* Asegúrate de tener mx-auto */}
+                    <Nav.Link 
+                        href="/" 
+                        onClick={() => handleNavLinkClick('home')} 
+                        className={activeLink === 'home' ? styles.active : ''}>
+                        Home
+                    </Nav.Link>
+                    <Nav.Link 
+                        href="/sale" 
+                        onClick={() => handleNavLinkClick('comprar')} 
+                        className={activeLink === 'comprar' ? styles.active : ''}>
+                        Comprar
+                    </Nav.Link>
+                    <Nav.Link 
+                        href="/sell" 
+                        onClick={() => handleNavLinkClick('vender')} 
+                        className={activeLink === 'vender' ? styles.active : ''}>
+                        Vender
+                    </Nav.Link>
+                    <Nav.Link 
+                        href="/rent" 
+                        onClick={() => handleNavLinkClick('alquilar')} 
+                        className={activeLink === 'alquilar' ? styles.active : ''}>
+                        Alquilar
+                    </Nav.Link>
+                    <Nav.Link 
+                        href="/desarrollo" 
+                        onClick={() => handleNavLinkClick('desarrollo')} 
+                        className={activeLink === 'desarrollo' ? styles.active : ''}>
+                        Desarrollos
+                    </Nav.Link>
+                    <Nav.Link 
+                        href="/franquicias" 
+                        onClick={() => handleNavLinkClick('franquicias')} 
+                        className={activeLink === 'franquicias' ? styles.active : ''}>
+                        Franquicias
+                    </Nav.Link>
+                    <Nav.Link 
+                        href="/contact" 
+                        onClick={() => handleNavLinkClick('contacto')} 
+                        className={activeLink === 'contacto' ? styles.active : ''}>
+                        Contacto
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
 
 export default NavBar;
