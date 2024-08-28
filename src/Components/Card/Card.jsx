1ed1155a0b4f5b9374b0accfe8ec81a570ed5f68
agustinrosa22 <agustinrosa1234@gmail.com>
2024-08-28 16:08:04 -0300

    changesCarrusellCard

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -9,6 +9,10 @@ import m2Casa from '../../Assets/metro-cuadrado.png';
 import m2 from '../../Assets/metro.png';
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
 const Card = (props) => {
   const [contact, setContact] = useState(null);
@@ -28,7 +32,7 @@ const Card = (props) => {
         if (response) {
           setContact(response.data.data);
         }
-        console.log(response);
+        // console.log(response);
       } catch (error) {
         console.error('Error al obtener los datos de contacto:', error);
       }
@@ -37,49 +41,42 @@ const Card = (props) => {
     fetchContact();
   }, [props.martillerId, props.sellerId]);
 
-  useEffect(() => {
-    const adjustImages = () => {
-      const imageContainers = document.querySelectorAll('.imageContainer');
-      imageContainers.forEach(container => {
-        const image = container.querySelector('img');
-        const containerAspectRatio = container.clientWidth / container.clientHeight;
-        const imageAspectRatio = image.naturalWidth / image.naturalHeight;
 
-        if (imageAspectRatio > containerAspectRatio) {
-          image.style.width = 'auto';
-          image.style.height = '100%';
-        } else {
-          image.style.width = '100%';
-          image.style.height = 'auto';
-        }
-
-        if (image.clientWidth < container.clientWidth || image.clientHeight < container.clientHeight) {
-          image.style.backgroundColor = '#ccc';
-        }
-      });
-    };
-
-    window.addEventListener('load', adjustImages);
-
-    return () => {
-      window.removeEventListener('load', adjustImages);
-    };
-  }, []);
+  const photos = props.photo
+  console.log(photos);
+  
 
   return (
     <div className={styles.card}>
-      <div className={styles.imageContainer}>
-        <Link to={`/detail/${props.id}`} className={styles.link}>
-          <Carousel>
-            {props.photo &&
-              props.photo.map((image, index) => (
-                <Carousel.Item key={index}>
-                  <img src={image} alt={`Slide ${index}`} className={styles.image} />
-                </Carousel.Item>
-              ))}
-          </Carousel>
-        </Link>
-      </div>
+     <div className={styles.imageContainer}>
+
+    {Array.isArray(photos) && photos.length > 0 ? (
+      <Swiper
+      modules={[Autoplay]}
+      autoplay={{
+        delay: 3000, // 3 segundos de retraso entre diapositivas
+        disableOnInteraction: false, // El autoplay no se detiene al interactuar
+      }}
+      loop={true} // Permite que el carrusel se repita en bucle
+      
+    >
+        {photos.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className={styles.imageContainer}>
+              <img
+                src={image}
+                className={styles['carousel-img']}
+                alt={`Slide ${index}`}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    ) : (
+      <p>No hay imágenes disponibles.</p>
+    )}
+  
+</div>
       <div className={styles.detailsCardContainer}>
         <Link to={`/detail/${props.id}`} className={styles.link}>
           <p className={styles.price}>{props.price} {props.currency}</p>
